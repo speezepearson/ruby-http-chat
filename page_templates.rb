28cff1650ca23670ad20a15617caf0d1968842ec
Spencer Pearson <speeze.pearson@gmail.com>
2013-06-02 09:25:07 -0700

    Reworked page-template system to abstract the process more from serve.rb

--- page_templates.rb
+++ page_templates.rb
@@ -4,12 +4,33 @@
 #  format them correctly, I think because I'm not clear what scope of
 #  variable names the <% commands %> have access to.
 PAGE_TEMPLATES = {:index => IO.read("index.html.erb"),
-                  :user_list => IO.read("user_list.html.erb"),
-                  :channel_list => IO.read("channel_list.html.erb"),
-                  :post_list => IO.read("post_list.html.erb")}
+                  :list => IO.read("list.html.erb"),
+                  :posts => IO.read("posts.html.erb"),
+                  :status => IO.read("status.html.erb")}
 
-def make_page(symbol, content)
+def make_page(symbol, options={})
   # Insert the content into the requested page template.
   # This will be obsolete once I figure out ERB scoping.
-  return PAGE_TEMPLATES[symbol].sub("<% CONTENT %>", content.to_s)
+  result = String.new(PAGE_TEMPLATES[symbol])
+  options.each do |key, value|
+    value = value.join("<br />") if value.instance_of? Array
+    result.sub!("<% #{key.to_s} %>", value.to_s)
+  end
+  return result
+end
+
+def index_page
+  return make_page(:index)
+end
+
+def list_page(title, content)
+  return make_page(:list, :title => title, :content => content)
+end
+
+def posts_page(chname, posts)
+  return make_page(:posts, :title => "Posts in #{chname}", :content => posts)
+end
+
+def status_page(content)
+  return make_page(:status, :content => content)
 end
--- serve.rb
+++ serve.rb
@@ -12,82 +12,82 @@ server = Server.new(Sequel.connect("sqlite://server.db"))
 
 # ...the main page:
 get "/" do
-  return make_page(:index, nil)
+  return index_page
 end
 
 # ...an attempted username registration:
 post "/register" do
   uname, password = params[:uname], params[:password]
-  return "no username given" if uname.nil? or uname.empty?
-  return "user #{uname} exists" if server.user_exists? uname
+  return status_page("no username given") if uname.nil? or uname.empty?
+  return status_page("user #{uname} exists") if server.user_exists? uname
   server.create_user(uname, password)
   session[:uname], session[:password] = uname, password
-  return "registration successful"
+  return status_page("registration successful")
 end
 # ...creating a channel:
 post "/channels" do
   chname = params[:chname]
-  return "no channel name given" if chname.nil? or chname.empty?
-  return "channel #{chname} exists" if server.channel_exists? chname
+  return status_page("no channel name given") if chname.nil? or chname.empty?
+  return status_page("channel #{chname} exists") if server.channel_exists? chname
   server.create_channel chname
-  return "created channel #{chname}"
+  return status_page("created channel #{chname}")
 end
 # ...making a post:
 post "/posts" do
   chname, content = session[:chname], params[:content]
-  return "no channel joined" if chname.nil? or chname.empty?
-  return "channel #{chname} does not exist" if !server.channel_exists? chname
-  return "no content given" if content.nil? or content.empty?
+  return status_page("no channel joined") if chname.nil? or chname.empty?
+  return status_page("channel #{chname} does not exist") if !server.channel_exists? chname
+  return status_page("no content given") if content.nil? or content.empty?
   uname = session[:uname]
   password = session[:password]
-  return "not logged in" if uname.nil?
-  return "invalid user info" if !server.password_correct?(uname, password)
+  return status_page("not logged in") if uname.nil?
+  return status_page("invalid user info") if !server.password_correct?(uname, password)
 
   server.new_post(chname, uname, params[:content])
-  return "post successful"
+  return status_page("post successful")
 end
 
 # ...logging in:
 get "/login" do
   uname, password = params[:uname], params[:password]
-  return "no username given" if uname.nil? or uname.empty?
-  return "already logged in as #{uname}" if session[:uname] == uname
-  return "invalid user info" if !server.password_correct?(uname, password)
+  return status_page("no username given") if uname.nil? or uname.empty?
+  return status_page("already logged in as #{uname}") if session[:uname] == uname
+  return status_page("invalid user info") if !server.password_correct?(uname, password)
   session[:uname] = uname
   session[:password] = password
-  return "logged in as #{uname}"
+  return status_page("logged in as #{uname}")
 end
 # ...joining a channel:
 get "/join_channel" do
   chname = params[:chname]
-  return "no channel name given" if chname.nil? or chname.empty?
-  return "no such channel" if !server.channel_exists? chname
+  return status_page("no channel name given") if chname.nil? or chname.empty?
+  return status_page("no such channel") if !server.channel_exists? chname
   session[:chname] = chname
-  return "joined channel #{chname}"
+  return status_page("joined channel #{chname}")
 end
 
 # ...a list of channels:
 get "/channels" do
-  return make_page(:channel_list, server.list_chnames.join("<br />"))
+  return list_page("All channels:", server.list_chnames)
 end
 # ...a list of users:
 get "/users" do
-  return make_page(:user_list, server.list_unames.join("<br />"))
+  return list_page("All users:", server.list_unames)
 end
 # ...a list of posts:
 get "/posts" do
   chname = session[:chname]
-  return "no channel" if chname.nil? or chname.empty?
-  return "no such channel" if !server.channel_exists? chname
+  return status_page("no channel") if chname.nil? or chname.empty?
+  return status_page("no such channel") if !server.channel_exists? chname
   start, stop = 0, -1
   begin
     start = Integer(params[:start]) if !(params[:start].nil? or params[:start].empty?)
     stop = Integer(params[:stop]) if !(params[:stop].nil? or params[:stop].empty?)
   rescue ArgumentError
-    return "bad start/stop value"
+    return status_page("bad start/stop value")
   end
 
   posts = server.get_posts(chname, start, stop)
-  return make_page(:post_list, posts.join("<br />"))
+  return posts_page(chname, posts)
 end
 
